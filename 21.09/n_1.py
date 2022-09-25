95eec6536db91fe4963ec59927131d3b0504af10
aminealist <aleshkov.d.o@gmail.com>
2022-09-25 18:43:43 +0700

    Update n_1.py

--- 21.09/n_1.py
+++ 21.09/n_1.py
@@ -1,7 +1,7 @@
 import re
 
 
-a='heopfheihf heofih 7 897 9909 щшуарущра аааолол adfdfau A.B.C.D.'
+a='heopfheihf heofih 7 897 9909 щшуарущра аааолол. A46л 4789745кгук Ouoegvbebищмиумю. adfdfau A.B.C.D.'
 
 
 with open ('n_1.txt', 'w+') as my_file:
@@ -10,4 +10,29 @@ with open ('n_1.txt', 'w+') as my_file:
 with open ('n_1.txt', 'r') as my_file:
     a = my_file.readline()
 
-print(re.search(r'^[уеэоаыяиюЮИЯЭОАЫЕУ]', a))
+a=' ' + a + ' '
+
+for i in range (a.count(' ')):
+
+    if (re.search(r'[ ][QWERTYUIOPASDFGHJKLZXCVBNMЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ.]+[ ]', a)) != None:
+        k = (re.search(r'[ ][QWERTYUIOPASDFGHJKLZXCVBNMЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ.]+[ ]', a)).group()
+        a = a.replace(((re.search(r'[ ][QWERTYUIOPASDFGHJKLZXCVBNMЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ.]+[ ]', a)).group()), ' @')
+print(a)
+gg = a
+
+for i in range (a.count(' ')):
+
+    if (re.search(r'[ ][уеэоаыяиюЮИЯЭОАЫЕУAaEeYyUuOo]+\w+', a)) != None:
+        k = (re.search(r'[ ][уеэоаыяиюЮИЯЭОАЫЕУAaEeYyUuOo]+\w+', a)).group()
+        print(k)
+        a = a.replace(((re.search(r'[ ][уеэоаыяиюЮИЯЭОАЫЕУAaEeYyUuOo]+\w+', a)).group()), ' ')
+
+for i in range (a.count(' ')):
+
+    if (re.search(r'[ ][1234567890]+[ ]', a)) != None:
+        k = (re.search(r'[ ][1234567890]+[ ]', a)).group()
+        print(k)
+        a = a.replace(((re.search(r'[ ][1234567890]+[ ]', a)).group()), ' ')
+
+dd = re.split(r'[.,/?!:]\s[QWERTYUIOPASDFGHJKLZXCVBNMЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ]', gg)
+print(dd)
