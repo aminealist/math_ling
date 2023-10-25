828b9f18dbd0c63a3a66b07fa3d1200afe2b5ed5
aminealist <113541691+aminealist@users.noreply.github.com>
2023-10-25 15:17:56 +0700

    25_10_2023

--- 2023-2024/ООП/03_10_2023/Figure/Figure_out.py
+++
@@ -1,6 +0,0 @@
-from Figure import Figure, Ellipse, Square
-
-s1 = Square(4)
-s1.parameters()
-s1.change_color("Красный")
-s1.parameters()
--- 2023-2024/ООП/10_10_2023/Banc_ac/BankAcc_out.py
+++
@@ -1,8 +0,0 @@
-from BancAccount import BankAccount
-
-Average_CPP_enjoyer_BankAcc = BankAccount("Alexander Grekhov", "1941818", 99999999, "Sagiman228")
-
-Average_CPP_enjoyer_BankAcc.show_me_balance()
-
-
-Average_CPP_enjoyer_BankAcc.i_need_money()
