a1f2ae3c28cf40fb0ca95a05a14501b2651f8871
aminealist <113541691+aminealist@users.noreply.github.com>
2023-10-25 15:14:07 +0700

    25_10_2023

---
+++ 2023-2024/ООП/Creating_classes/3/Shapes_out.py
@@ -0,0 +1,16 @@
+from Shapes import Ellipse, N_gon
+
+ellipsis1 = Ellipse(1, 1)
+ellipsis2 = Ellipse(1, 2)
+print(f"Это круг? {ellipsis1.is_circle()}")
+print(f'Площадь {ellipsis2.area()}')
+print(f"Эксцентриситет {ellipsis1.eccentricity()}")
+
+triangle = N_gon(3, [1, 2, 3], [60, 90, 30])
+print(f"Треугольник вырожденый? {triangle.is_degenerate()}")
+
+square = N_gon(4, [2, 2, 2, 2], [90] * 4)
+print(f"Четырёхугольник выпуклый? {square.is_convex()}")
+
+pentagon = N_gon(5, [2, 4, 5, 6, 7], [0] * 5)
+print(f"Периметр пятиугольника {pentagon.c_perimeter()}")
